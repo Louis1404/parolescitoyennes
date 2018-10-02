9f9cc7680c47556a7a87b8f985f964bc3ec085e9
louisburette <louis.burette@ieseg.fr>
2018-10-02 11:39:49 +0200

    routes debugging

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,14 +1,28 @@
 class VideosController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
   def index
+    @videos = Video.all
   end
 
   def new
+    @video = Video.new
   end
 
   def create
+    @video = Video.new(video_params)
+    if @video.save
+      redirect_to video_path(@video)
+    else
+      render :new
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def video_params
+    params.require(:video).permit(:title, :url)
   end
 end
