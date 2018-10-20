d5f21654b3c3e1d486299f3478fb377b0b5fe62c
louisburette <louis.burette@ieseg.fr>
2018-10-20 15:22:02 +0200

    content in progress

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -6,13 +6,13 @@ class MessageMailer < ApplicationMailer
   #   en.message_mailer.contact_me.subject
   #
   def contact_me(message)
-      @prénom = message.prenom
-      @nom = message.nom
-      @email = message.email
-      @téléphone = message.telephone
-      @titre = message.titre
-      @contenu = message.contenu
+    @prénom = message.prenom
+    @nom = message.nom
+    @email = message.email
+    @téléphone = message.telephone
+    @titre = message.titre
+    @contenu = message.contenu
 
-      mail to: "louisburette14@gmail.com", from: message.email, subject: "Paroles Citoyennes Villeneuve 2020"
-    end
+    mail to: "parolescitoyennes.vda2020@gmail.com", from: message.email, subject: "Paroles Citoyennes Villeneuve 2020"
+  end
 end
