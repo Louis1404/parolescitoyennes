427f6b954989d45a8204f8573b609485f4c82855
louisburette <louis.burette@ieseg.fr>
2018-10-25 21:03:08 +0200

    pages mentions légales

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, :skip => [:registrations], controllers: { sessions: 'users/sessions' }
   root to: 'videos#index'
   get 'quinoussommes', to: 'pages#quinoussommes', as: :quinoussommes
+  get 'mentionslegales', to: 'pages#mentionslegales'
   get 'proposition', to: 'messages#new', as: :new_proposition
   post 'proposition', to: 'messages#create', as: :create_proposition
   resources :videos, except: [:show, :edit, :update]
