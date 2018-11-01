f13fdc608e8dc1a70c1b86e6c075a1057ab67af5
louisburette <louis.burette@ieseg.fr>
2018-11-01 20:24:18 +0100

    debug

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
+
 //import swal from 'sweetalert';
-//import "bootstrap";
-=======
 import "bootstrap";
->>>>>>> b1017436418b399ae0ab53f06d69502d380258be
+
+
 
 /* eslint no-console:0 */
 // This file is automatically compiled by Webpack, along with any other files
